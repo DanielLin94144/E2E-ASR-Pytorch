6f7f40e6754b5302490d55d7b43c20b4d88811db
darongliu <givebirthday.repeat@gmail.com>
2021-02-11 22:04:25 +0800

    copy original asr script as trainable-aug init script

---
+++ script/train_babel_trainable_aug.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# $1 : experiment name
+# $2 : cuda id
+
+CONFIG="babel_202_trainable_aug.yaml"
+
+DIR="/home/darong/other_storage/e2e_model"
+
+echo "Start running training process of E2E ASR"
+CUDA_VISIBLE_DEVICES=$2 python3 main.py --config config/${CONFIG}.yaml \
+    --name $1 \
+    --njobs 16 \
+    --seed 0 \
+    --logdir ${DIR}/log/ \
+    --ckpdir ${DIR}/ckpt/ \
+    --outdir ${DIR}/result/ \
+    --reserve_gpu 0 \
+    --babel babel \
+    --no_cudnn \
+
